0ef6cfe4f6f6c4d1b7ab2063a0289df843052941
D. R <sollunaterra@yahoo.com.pe>
2020-10-17 21:07:34 -0500

    Add files via upload
    crud_django_bootstrap
    
    Client MYSQL with Django (included)
    pip install mysqlclient-1.4.2-cp37-cp37m-win32.whl
    pip install mysqlclient-1.4.2-cp37-cp37m-win_amd64.whl
    
    
    > django-bootstrap4
    pip install django-bootstrap4
    https://pypi.org/project/django-bootstrap4/
    
    > render & manage the fields of the forms 
    pip install django-widget-tweaks
    https://pypi.org/project/django-widget-tweaks/
    
    > clear cache completely
    pip install django-clear-cache
    https://pypi.org/project/django-clear-cache/
    
    
    
    
    python app/manage.py makemigrations postres
    python manage.py makemigrations postres
    
    python app/manage.py migrate postres
    python manage.py migrate postres
    
    python app/manage.py migrate
    python manage.py migrate
    
    
    python app/manage.py runserver 
    python manage.py runserver 
    
    http://127.0.0.1:8000/admin/
    
    http://127.0.0.1:8000/postres/
    
    Check:
    
    > Python Version
    python -V
    
    > Django Version
    python -c "import django; print(django.get_version())"

---
+++ crud_django_bootstrap/settings.py
@@ -0,0 +1,124 @@
+"""
+Django settings for crud_django_bootstrap project.
+"""
+
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'h&-9zg!afl0sv!3t%r^9ud(6=$0e013^82_eg&mig2&8pp9u#k'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'postres',
+    'clear_cache',
+    'bootstrap4',
+    'widget_tweaks',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'crud_django_bootstrap.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'crud_django_bootstrap.wsgi.application'
+
+
+DATABASES = {
+    'sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'productos',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
+    }
+}
+
+
+# Password validation
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/' 
+
+MEDIA_URL = '/postres/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'postres/static/uploads')
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage' 
